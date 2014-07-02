1271f957b556b99f79b3752f6a40cdf88ad29397
DanielBoerlage <daniel.brlg@gmail.com>
2014-07-02 01:29:00 -0700

    moved Globbed to be nested in Input.java

--- src/muplr/Input.java
+++ src/muplr/Input.java
@@ -3,6 +3,13 @@ package muplr;
 import java.util.Properties;
 import java.lang.IllegalArgumentException;
 import java.lang.NumberFormatException;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileSystems;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.io.IOException;
 
 public class Input {
 
@@ -41,5 +48,26 @@ public class Input {
 		System.out.println(properties.getProperty("repeat","false"));
 		System.out.println(properties.getProperty("volume","100"));
 		System.out.println(Main.workingDirectory);
+	}
+
+	private class Globber extends SimpleFileVisitor<Path> {
+
+		private final PathMatcher pathMatcher;
+
+		Globber(String pattern) {
+     	   pathMatcher = FileSystems.getDefault().getPathMatcher("glob:" + pattern);
+  	 	}
+
+ 	 	@Override
+  		public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+  		  	// do stuff
+        	return FileVisitResult.CONTINUE;
+  		}
+
+    	@Override
+    	public FileVisitResult visitFileFailed(Path file, IOException e) {
+        	Output.printErr(e.getMessage());
+        	return FileVisitResult.CONTINUE;
+    	}
 	}
 }
